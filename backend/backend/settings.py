b481f63197abd4e042b0955a8c7cf8c2b4f2ff61
Mertcanp67 <mertcanp603@gmail.com>
2025-11-20 19:26:59 +0300

    api url duzeltildi ve cors eklendi

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -115,6 +115,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",   
     "http://127.0.0.1:5174",   
+    "https://notdjango.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
