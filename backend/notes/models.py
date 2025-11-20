54521f6bd0f6fa1c1af2763f972a41bbc8314061
Mertcanp67 <mertcanp603@gmail.com>
2025-11-21 02:13:21 +0300

    s

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -18,6 +18,7 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True, null=True) 
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
     tags = TaggableManager(blank=True)
     is_private = models.BooleanField(default=False) 
 
